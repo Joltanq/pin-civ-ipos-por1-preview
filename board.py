73b42f8043a0fef9f78f343c845f5fd44cd6cb2a
joelt <joel.tan.92@gmail.com>
2025-08-02 01:47:25 +0800

    create board class

---
+++ board.py
@@ -0,0 +1,8 @@
+class Board:
+    def __init__(self,size):
+        self.size = size
+
+    # needs functions to
+    # update board with move
+    # check win status
+    # check for tie? maybe this is the same as win state?
